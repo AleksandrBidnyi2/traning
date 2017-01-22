f8e59ed4b927d042acc3e9a12c2520d9ab7c6153
Aleksandr Bednyi <nevergiveupman9@gmail.com>
2017-01-22 20:47:15 +0200

    four commit

--- src/Homework1.java
+++ src/Homework1.java
@@ -4,7 +4,7 @@ public class Homework1 {
 
         Class1 class1 = new Class1();//first homework
         Class2 class2 = new Class2();// more text for learning git
-        Class3 class3 = new Class3();
+        Class3 class3 = new Class3();// four commit
         Class4 class4 = new Class4();
         Class5 class5 = new Class5();
 
